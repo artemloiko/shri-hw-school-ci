33d011bc2b7277ea76691bc442fe306ef44f51c6
Artem Loiko <artuom130@gmail.com>
2020-03-06 19:13:33 +0300

    Init default build

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+
+const cssFileOrder = ['src/reset.css', 'src/index.css', 'src/**/*.css'];
+
+function build() {
+  return src(cssFileOrder)
+    .pipe(concat('style.css'))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('dist'))
+    .pipe(src('pages/*.html'))
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream());
+}
+
+function buildProd() {
+  return src(cssFileOrder)
+    .pipe(concat('style.css'))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('build'))
+    .pipe(src('pages/*.html'))
+    .pipe(dest('build'));
+}
+
+function watchFiles() {
+  browserSync.init({ server: './dist', notify: false });
+  watch('src/**/*.css', build);
+  watch('pages/*.html', build).on('change', browserSync.reload);
+}
+
+exports.default = build;
+exports.build = buildProd;
+exports.watch = series(build, watchFiles);
