b854fc65672c4239390968042dd96dc68870cabc
Artem Loiko <artuom130@gmail.com>
2020-03-13 18:09:28 +0300

    Add base app config

---
+++ server/config/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+const envFound = dotenv.config();
+if (!envFound) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  port: process.env.PORT || '3000',
+  storageURL: process.env.STORAGE_URL,
+  storageApikey: process.env.STORAGE_API_KEY,
+};
