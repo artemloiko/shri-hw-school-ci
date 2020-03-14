a1dcb3fd982dac2ed200d272aa0f90b51532cc7a
Artem Loiko <artuom130@gmail.com>
2020-03-14 20:40:35 +0300

    Fix routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const apiRoutes = require('./api');
 
 const init = (server) => {
-  server.use(express.static(path.resolve(__dirname, '../../build')));
+  server.use(express.static(path.resolve(__dirname, '../../client/build')));
 
   server.use('/api', apiRoutes);
 
   server.get('/*', (req, res) => {
-    const pathToFile = path.resolve(__dirname, '../../build/404.html');
+    const pathToFile = path.resolve(__dirname, '../../client/build/404.html');
     res.status(404);
     return res.sendFile(pathToFile);
   });
