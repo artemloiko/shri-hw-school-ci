3572aa79e5886c18c630fc5f92c661223a13ec31
Artem Loiko <artuom130@gmail.com>
2020-03-16 13:25:58 +0300

    Add process failing without .env

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv');
 
 const envFound = dotenv.config();
-if (!envFound) {
+
+if (envFound.error) {
   // This error should crash whole process
 
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
