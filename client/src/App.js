3b0726bc71417435d8a6971f465d43b4e6e37d9a
Artem Loiko <artuom130@gmail.com>
2020-03-24 21:55:10 +0300

    Add css blocks

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Home from './pages/Home/Home';
+import Settings from './pages/Settings/Settings';
+
+function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      <Settings />
+    </>
+  );
+}
+
+export default App;
