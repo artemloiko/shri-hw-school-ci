21f99494bbcd8cb0c5b3930d97c6b7f0093dad28
Artem Loiko <artuom130@gmail.com>
2020-03-28 19:10:55 +0300

    Add base component Modal

---
+++ client/src/components/base/Modal/Modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
+
+import { cn } from 'utils/bem-cn';
+
+import './Modal.css';
+
+ReactModal.setAppElement('#root');
+
+function Modal(props) {
+  const { children } = props;
+  return (
+    <ReactModal
+      {...props}
+      className={cn('modal', { props })}
+      overlayClassName="modal-overlay typography"
+    >
+      {children || 'Example Modal'}
+    </ReactModal>
+  );
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  childre: PropTypes.any,
+};
+
+export default Modal;
