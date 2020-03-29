4b336eaf42b3fe0ae7a4c503cb44464f2f58f0be
Artem Loiko <artuom130@gmail.com>
2020-03-29 18:39:55 +0300

    Add rebuild button to details page

--- client/src/pages/Details/Details.js
+++ client/src/pages/Details/Details.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { getBuildDetails } from 'actions/BuildsDetailsAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { navigate } from '@reach/router';
 
 import Page from 'components/common/Page/Page';
 import CardCiRun from 'components/common/CardCiRun/CardCiRun';
 import Log from 'components/common/Log/Log';
 import Loader from 'components/common/Loader/Loader';
-import { useDispatch, useSelector } from 'react-redux';
+import Button from 'components/base/Button/Button';
+import Icon from 'components/base/Icon/Icon';
+
+import { getBuildDetails } from 'actions/BuildsDetailsAction';
+import { updateBuildsList, addBuild } from 'actions/BuildsAction';
 
 import './Details.css';
 
@@ -21,9 +26,43 @@ function Details(props) {
   const currentBuild = useSelector((state) => state.buildsDetails[buildId]);
 
   const isDetailsLoading = !currentBuild || !currentBuild.details.isLoaded;
+  const handleRebuild = async () => {
+    const commitHash = currentBuild?.details?.commitHash;
+    if (!commitHash) return;
+    try {
+      const data = await addBuild(currentBuild.details.commitHash);
+      dispatch(updateBuildsList(data));
+      navigate(`/details/${data.id}`);
+    } catch (error) {
+      console.log('Cannot find commit', commitHash);
+    }
+  };
 
   return (
-    <Page contentClass="details">
+    <Page
+      contentClass="details"
+      headerControls={
+        <>
+          <Button
+            type="button"
+            className="header__control"
+            mods={{ size: 'small', icon: true }}
+            icon={<Icon mods={{ size: 'small', type: 'rebuild' }} />}
+            onClick={handleRebuild}
+          >
+            Rebuild
+          </Button>
+          <Button
+            to="/settings"
+            className="header__control"
+            mods={{ size: 'small', icon: true, 'icon-only': true }}
+            icon={<Icon mods={{ size: 'small', type: 'settings' }} />}
+          >
+            Settings
+          </Button>
+        </>
+      }
+    >
       <Loader isLoading={isDetailsLoading} mods={{ animate: true }}>
         <div className="container container_shrink">
           {currentBuild?.details && (
