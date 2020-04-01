717d7b7a7b940ab76c7ba21662ebdb1a03a70574
Artem Loiko <artuom130@gmail.com>
2020-04-01 12:33:20 +0300

    Delete page test

--- client/src/components/common/Page/Page.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Page from './Page';
-
-test('renders page header', () => {
-  const { getByText } = render(<Page />);
-  const header = getByText(/School CI server/i);
-  expect(header).toBeInTheDocument();
-});
