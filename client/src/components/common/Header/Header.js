c084f1b098d8e2b8c62e7f18c1c11c083341f9ca
Artem Loiko <artuom130@gmail.com>
2020-04-01 15:40:32 +0300

    Fix headings typography

--- client/src/components/common/Header/Header.js
+++ client/src/components/common/Header/Header.js
@@ -5,14 +5,14 @@ import { Link } from '@reach/router';
 import './Header.css';
 
 function Header(props) {
-  const { text = ' School CI server', controls } = props;
+  const { text, controls } = props;
 
   return (
     <header className="header">
       <div className="container header__container">
         <h1 className="header__heading">
           <Link to="/" className={text ? 'typography__headline2' : 'typography__headline1'}>
-            {text}
+            {text || 'School CI server'}
           </Link>
         </h1>
         <div className="header__control-group">{controls}</div>
