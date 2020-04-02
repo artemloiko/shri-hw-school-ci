e045175abe7bc33c5b30b0a96568540cc53bea25
Artem Loiko <artuom130@gmail.com>
2020-04-02 18:57:03 +0300

    Add BuildForm (splitted BuildModal - form & modal)

--- client/src/pages/Home/components/BuildModal/BuildModal.js
+++ client/src/pages/Home/components/BuildModal/BuildModal.js
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { navigate } from '@reach/router';
-import { Formik } from 'formik';
 
 import Modal from 'components/base/Modal/Modal';
-import Input from 'components/base/Input/Input';
-import Button from 'components/base/Button/Button';
-import Loader from 'components/common/Loader/Loader';
-import ErrorModal from 'components/common/ErrorModal/ErrorModal';
-
-import { addBuild, updateBuildsList } from 'actions/BuildsAction';
+import BuildForm from 'pages/Home/components/BuildForm/BuildForm';
 
 import './BuildModal.css';
 
 function BuildModal(props) {
   const { closeModal, isOpen } = props;
 
-  const dispatch = useDispatch();
-  const [error, setError] = useState({ isOpen: false });
   return (
     <Modal
       isOpen={isOpen}
@@ -30,72 +20,7 @@ function BuildModal(props) {
       <p className="typography__body2 build-modal__elem">
         Enter the commit hash which you want to build.
       </p>
-      <Formik
-        initialValues={{
-          commitHash: '',
-        }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.commitHash) {
-            errors.period = 'Commit hash is required';
-          }
-          return errors;
-        }}
-        onSubmit={async (values, { setSubmitting, resetForm }) => {
-          try {
-            const data = await addBuild(values.commitHash);
-            dispatch(updateBuildsList(data));
-            navigate(`/details/${data.id}`);
-          } catch (error) {
-            setError({ isOpen: true, message: error?.response?.data?.error.message });
-          }
-          resetForm();
-          setSubmitting(false);
-        }}
-      >
-        {(formikBag) => {
-          const { getFieldProps, handleSubmit, isSubmitting, setFieldValue } = formikBag;
-          return (
-            <Loader isLoading={isSubmitting} showContent style={{ width: '30%' }}>
-              <form className="build-modal__form build-modal__elem" onSubmit={handleSubmit}>
-                <Input
-                  mods={{ clear: true, fullwidth: true }}
-                  placeholder="Commit hash"
-                  id="commitHash"
-                  setFieldValue={setFieldValue}
-                  {...getFieldProps('commitHash')}
-                  required
-                  pattern="\w{6,}"
-                  title="Hash 6 letters/numbers"
-                ></Input>
-                <div className="build-modal__form-submit-group build-modal__elem">
-                  <Button
-                    type="submit"
-                    mods={{ action: true, disabled: isSubmitting }}
-                    className="build-modal__form-submit-group-elem"
-                  >
-                    Run build
-                  </Button>
-                  <Button
-                    className="build-modal__form-submit-group-elem"
-                    mods={{ disabled: isSubmitting }}
-                    onClick={closeModal}
-                  >
-                    Cancel
-                  </Button>
-                </div>
-              </form>
-            </Loader>
-          );
-        }}
-      </Formik>
-      <ErrorModal
-        isOpen={error.isOpen}
-        errorMessage={error.message}
-        closeModal={() => {
-          setError({ isOpen: false });
-        }}
-      ></ErrorModal>
+      <BuildForm closeModal={closeModal} />
     </Modal>
   );
 }
