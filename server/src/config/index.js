d4b9e2f1ff200f0c0bbdb0de7c8887659b80efd7
Artem Loiko <artuom130@gmail.com>
2020-04-10 16:41:51 +0300

    Add GitOutputParser with tests Refactor path using

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -8,10 +8,12 @@ if (envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.REPO_PATH = 'repo';
 
 module.exports = {
   nodeEnv: process.env.NODE_ENV,
   port: process.env.PORT || '3000',
   storageURL: process.env.STORAGE_URL,
   storageApikey: process.env.STORAGE_API_KEY,
+  repoPath: process.env.REPO_PATH,
 };
