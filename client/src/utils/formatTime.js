6317bcd413d4de6a683a26234a54bf02f9fde0e2
Artem Loiko <artuom130@gmail.com>
2020-04-16 22:03:39 +0300

    Fix formatTime function for tiny values

--- client/src/utils/formatTime.js
+++ client/src/utils/formatTime.js
@@ -7,5 +7,6 @@ export const formatTime = (duration) => {
   const durationInMinutes = Math.round(duration / 60);
   const minutes = durationInMinutes % 60;
   const hours = (durationInMinutes - minutes) / 60;
-  return hours ? `${hours} h ${minutes} min` : `${minutes} min`;
+  const minutesToShow = minutes || (duration / 60).toFixed(1);
+  return hours ? `${hours} h ${minutes} min` : `${minutesToShow} min`;
 };
