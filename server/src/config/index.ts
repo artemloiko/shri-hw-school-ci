1712197b6d94d948a9ba4e0c668093f04491a6ab
Artem Loiko <artuom130@gmail.com>
2020-04-28 12:13:20 +0300

    Update server configuring

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,22 +1,21 @@
 import dotenv from 'dotenv';
-import findConfig from 'find-config';
-
-const envFile = process.env.NODE_TEST === 'test' ? '.env.test' : '.env';
-const pathToEnv = findConfig(envFile) || envFile;
-const envFound = dotenv.config({ path: pathToEnv });
+const envFound = dotenv.config();
 
 if (envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.REPO_PATH = 'repo';
 
 const config = {
   nodeEnv: process.env.NODE_ENV,
-  port: process.env.PORT || '3000',
-  storageURL: process.env.STORAGE_URL,
   storageApikey: process.env.STORAGE_API_KEY,
+  port: process.env.PORT || '3001',
   repoPath: process.env.REPO_PATH,
+  storageURL: process.env.STORAGE_URL || 'https://hw.shri.yandex/api',
 };
+
+if (!config.storageApikey) {
+  throw new Error('STORAGE_API_KEY is required');
+}
 
 export default config;
