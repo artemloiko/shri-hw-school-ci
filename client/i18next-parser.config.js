5cce60b9ceaf4931ace4e8f9712b294d8b15a9d4
Artem Loiko <artuom130@gmail.com>
2020-05-26 16:41:03 +0300

    Delete extra config

--- client/i18next-parser.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  defaultNamespace: 'common',
-  // Default namespace used in your i18next config
-  locales: ['en', 'ru'],
-  // An array of the locales in your applications
-};
