f681ed5063b7ae0d3a537827482a8f77572c78c4
Artem Loiko <artuom130@gmail.com>
2020-06-16 18:05:06 +0300

    Fix formatTime tests

--- client/src/__tests__/utils/formatTime.test.ts
+++ client/src/__tests__/utils/formatTime.test.ts
@@ -1,18 +1,24 @@
 import { formatTime } from '../../utils/formatTime';
+import i18n, { TFunction } from 'i18next';
 
 describe('formatTime', () => {
+  let t: TFunction;
+  beforeAll(async () => {
+    t = await i18n.init();
+  });
+
   test('returns formated duration with only seconds', () => {
-    const duration = formatTime(30);
+    const duration = formatTime(30, t);
 
     expect(duration).toBe('30 sec');
   });
   test('returns formated duration with only minutes', () => {
-    const duration = formatTime(600);
+    const duration = formatTime(600, t);
 
     expect(duration).toBe('10 min');
   });
   test('returns formated duration with hours and minutes', () => {
-    const duration = formatTime(6000);
+    const duration = formatTime(6000, t);
 
     expect(duration).toBe('1 h 40 min');
   });
